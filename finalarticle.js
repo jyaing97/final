c12eba9dfbf3b7f211d14017f2ffc1db4a812aad
jyaing97 <33529819+jyaing97@users.noreply.github.com>
2018-12-11 16:22:20 -0500

    Add files via upload

---
+++ finalarticle.js
@@ -0,0 +1,12 @@
+ $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+   $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+     $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
